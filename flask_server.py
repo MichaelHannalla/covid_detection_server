babbd2306be8485b7bb59a882a9cbbe17bcb794e
Michael Samy Hannalla <michael.sami.hanna@gmail.com>
2021-07-12 11:24:57 +0200

    added exception handling in main upload function

--- flask_server.py
+++ flask_server.py
@@ -9,7 +9,7 @@ import imghdr
 import os
 
 from utils import classify_crop, get_strip_crop, get_image_data
-from utils import positive, negative
+from utils import positive, negative, labels
 
 import torch
 
@@ -27,11 +27,13 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['MAX_CONTENT_LENGTH'] = 4096 * 4096 
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', 'jpeg']
 app.config['UPLOAD_PATH'] = 'uploads'
+os.environ['FLASK_APP'] = 'covid_detection_server'
+os.environ['FLASK_ENV'] = 'development'
 
 # Load the deep learning models
 print("SERVER LOADING, PLEASE WAIT....")
 global strip_detection_model, strip_classifier_model
-strip_detection_model = Model.load('models/covid_strip_weights_single_class.pth', ['strip'])
+strip_detection_model = Model.load('models/covid_strip_weights_single_class.pth', labels)
 strip_classifier_model = torch.load('models/strip_classifier_mini.pth')
 strip_classifier_model.eval()
 print("SERVER READY")
@@ -76,11 +78,15 @@ def upload_files():
         #         file_ext != validate_image(uploaded_file.stream):
         #     abort(400)
 
-        # Perform the detection on the incoming stream of image
-        img_cv = get_image_data(uploaded_file)                              # Get image from flask server
-        strip_crop = get_strip_crop(img_cv, strip_detection_model)          # Get area of interest (strip area)
-        result = classify_crop(strip_crop, strip_classifier_model)          # Classify the sample
-        send_output(result)                                                 # Send the output to flask server
+        try:
+            # Perform the detection on the incoming stream of image
+            img_cv = get_image_data(uploaded_file)                              # Get image from flask server
+            strip_crop = get_strip_crop(img_cv, strip_detection_model)          # Get area of interest (strip area)
+            result = classify_crop(strip_crop, strip_classifier_model)          # Classify the sample
+            send_output(result)    
+        except:
+            flash("Exception caught during runtime, check for invalid inputs")
+                                                         # Send the output to flask server
 
     return redirect(url_for('index'))
 
